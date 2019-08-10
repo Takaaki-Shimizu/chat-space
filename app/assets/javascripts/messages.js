19bcbcce14ce2c82a2f699f6bda60bf42cc0b108
清水隆亮 <shimizutakaaki@shimizutakashiryounoMacBook-Pro.local>
2019-08-10 21:13:39 +0900

    add-Automatic-updating

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,9 +1,9 @@
-$(document).on('turbolinks:load', function(){
+$(function(){
   // メッセージ表示のHTML生成
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var image_url = (message.image_url) ? `<image class="lower-message_image" src="${ message.image_url }">` : "";
-    var html = `<div class="message" id="${ message.id }">
+    var html = `<div class="message" data-message-id="${ message.id }">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${ message.name }
@@ -15,8 +15,8 @@ $(document).on('turbolinks:load', function(){
                   <div class="lower-message">
                     <p class="lower-message__content">
                       ${message.content}
-                      ${image_url}
                     </p>
+                      ${image_url}
                   </div>
                 </div>`
     return html;
@@ -47,4 +47,34 @@ $(document).on('turbolinks:load', function(){
       $('.form__submit').attr('disabled', false);
     })
   })
+
+//自動更新
+  $(function(){
+    setInterval(autoUpdate, 3000);
+  });
+  function autoUpdate() {
+    if (location.href.match(/\/groups\/\d+\/messages/)) {
+      var last_message_id = $('.message').last().data('message-id');
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          messages.forEach(function(message) {
+          var html = buildHTML(message);
+            $('.messages').append(html);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+          });
+        }
+       })
+       .fail(function(){
+        alert("自動メッセージ取得に失敗しました");
+      })
+    } else {
+      clearInterval(autoUpdate);
+    }
+  };
 });
